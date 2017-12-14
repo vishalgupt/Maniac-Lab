9aff13d34708242032f179c3ec8386e13a2bb5e1
vishal gupta <vishal.gupta.14cs@bml.edu.on>
2017-12-14 12:53:24 +0530

    maniac lab

---
+++ page16.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Identifying the customer choice</title>\
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/styles16.css">
+</head>
+
+<body>
+
+
+      
+      <nav class="navbar">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          
+          <a class="navbar-brand" href="#">Identifying the customer choice</a>
+
+          <div align="center" >
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a class="nav-link" href="#"> Demo <span class="sr-only">(current)</span></a>
+                </li>
+                
+                <li  class="list-inline-item">
+                    <a class="nav-link " href="#">Developers</a>
+                </li>
+                <li  class="list-inline-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li  class="list-inline-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li  class="list-inline-item">
+                    <a class="nav-link" href="#">Contact us</a>
+                </li>
+            
+                
+                  <li class="list-inline-item"><form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form></li>
+              </ul>
+
+          </div>
+      </nav>
+
+          <div class="container">
+          <center>
+          <div class="jumbotron">
+            
+            <h1> Designs Originals</h1>
+            <p>Whether you are the company owner or a developer, You have come to right place!</p>
+            <a class="btn btn-info"> Get Started</a>
+
+             </div>
+             </center>
+              <center><h1>WOMEN DRIVEN COMPANY LOGOS</h1></center>
+             
+
+             
+
+             <div class="design-section">
+               <ul class="design-grid">
+                 <li>
+                   <div class="design-box design-img-1">
+                     <a href="page19.php">
+                      <h3>Design1</h3>
+                      <p> </p>
+                     </a>
+                   </div>
+
+                 </li>
+
+                 <li>
+                   <div class="design-box design-img-2">
+                     <a href="page19.php">
+                      <h3>Design2</h3>
+                      <p> </p>
+
+                     </a>
+                   </div>
+
+                 </li>
+
+                 <li>
+                   <div class="design-box design-img-3" >
+                     <a href="page19.php">
+                      <h3>Design3</h3>
+                      <p></p>
+                  
+                     </a>
+                   </div>
+
+                 </li>
+
+                 <li>
+                   <div class="design-box design-img-4">
+                     <a href="page19.php">
+                      <h3>Design4</h3>
+                      <p> </p>
+                     </a>
+                   </div>
+
+                 </li>
+
+                 <li>
+                   <div class="design-box design-img-5">
+                     <a href="page19.php">
+                      <h3>Design5</h3>
+                      <p> </p>
+                     </a>
+                   </div>
+
+                 </li>
+
+                 <li>
+                   <div class="design-box design-img-6">
+                     <a href="page19.php">
+                      <h3>Design6</h3>
+                      <p> </p>
+                     </a>
+                   </div>
+
+                 </li>
+
+                   <li>
+                   <div class="design-box design-img-7">
+                     <a href="page20.php">
+                      <h3>Design7</h3>
+                      <p> </p>
+                     </a>
+                   </div>
+
+                 </li>
+
+                   <li>
+                   <div class="design-box design-img-8">
+                     <a href="page20.php">
+                      <h3>Design8</h3>
+                      <p> </p>
+                     </a>
+                   </div>
+
+                 </li>
+
+               </ul>
+
+               <!--
+
+               Motive Jumbotron
+
+               -->
+
+               <center>
+                <div class="jumbotron" id="motivejumbotron">
+                  
+                    <h1> Motive</h1>
+                    <p>Getting the right customer choice in the business is the first and most important thing to grab. This is the first priority and need. When it comes to know about customer preferences in digital products like website, Graphics, mobile apps etc, it becomes very difficult to actually convey one’s requirement in textual context. Hence we are developing an application identify customer prefernces by tagging logo with attributes of masculine ,femininity,youthful,mature,classic ,modern,geometric,organic,abstract,literal.</p>
+                    <a class="btn btn-info"> Get Started</a>
+                    </div>
+             </center>
+
+             </div>
+        </div>
+
+
+
+      
+
+
+<!--Jquery -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ page20.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Identifying the customer choice</title>\
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/styles20.css">
+</head>
+
+<body>
+
+
+      
+      <nav class="navbar">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          
+          <a class="navbar-brand" href="#">Identifying the customer choice</a>
+
+          <div align="center" >
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a class="nav-link" href="#"> Demo <span class="sr-only">(current)</span></a>
+                </li>
+               
+                <li  class="list-inline-item">
+                    <a class="nav-link " href="#">Developers</a>
+                </li>
+                <li  class="list-inline-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li  class="list-inline-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li  class="list-inline-item">
+                    <a class="nav-link" href="#">Contact us</a>
+                </li>
+              </ul>
+                
+                <form class="form-inline my-2 my-lg-0" >
+                  <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                
+
+          </div>
+      </nav>
+
+          <div class="container">
+          <center>
+          <div class="jumbotron">
+            
+            <h1> Logo Designs</h1>
+            <p>Whether you are the company owner or a developer, You have come to right place!</p>
+            <a class="btn btn-info"> Get Started</a>
+
+             </div>
+            
+
+             <div class="container">
+             <center>
+            <div class="jumbotron">
+                <h1 style="color:blue;">CUSTOMER PREFERENCE ANALYSIS  </h1>
+           
+            <h1 style="color:blue;">MODERN   =<span style="color: #fff;">45.8 %</span>&ensp;&ensp;  &ensp;               CLASSIC=<span style="color: #fff;">   54.2 %  </h1>
+            <h1 style="color:blue;">YOUTHFUL  =<span style="color: #fff;">45.8 %</span>&ensp;&ensp;  &ensp;              MATURE=<span style="color: #fff;"> 54.2 %    </h1>
+            <h1 style="color:blue;">ORGANIC =<span style="color: #fff;">45.8  %</span>&ensp;&ensp;  &ensp;              GEOMETRY=<span style="color: #fff;">  54.2 %   </h1>
+             <h1 style="color:blue;">ABSTRACT  =<span style="color: #fff;">45.8 %</span>&ensp;&ensp;  &ensp;                TEXT=<span style="color: #fff;"> 54.2 %      </h1>
+            </div>
+             </center>
+            
+
+               <!--
+
+               Motive Jumbotron
+
+               -->
+
+               <center>
+                <div class="jumbotron" id="motivejumbotron">
+                  
+                    <h1> Motive</h1>
+                    <p>Getting the right customer choice in the business is the first and most important thing to grab. This is the first priority and need. When it comes to know about customer preferences in digital products like website, Graphics, mobile apps etc, it becomes very difficult to actually convey one’s requirement in textual context. Hence we are developing an application identify customer prefernces by tagging logo with attributes of masculine ,femininity,youthful,mature,classic ,modern,geometric,organic,abstract,literal.</p>
+                    <a class="btn btn-info"> Get Started</a>
+                    </div>
+             </center>
+
+             </div>
+        </div>
+
+
+
+      
+
+
+<!--Jquery -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+</body>
+</html>
